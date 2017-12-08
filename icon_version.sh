8a02dcc4626371e7bafe171fe2c79491ed74d610
LuisX <15504222202@163.com>
2017-12-08 22:35:45 +0800

    update

--- icon_version.sh
+++ icon_version.sh
@@ -17,27 +17,21 @@ gsValidation=false;
 fi
 
 if [[ "$convertValidation" = true || "$gsValidation" = true ]]; then
-echo "提示: 如需icon版本化，请使用brew安装ImageMagick和ghostscript字体"
+echo "WARNING: Skipping Icon versioning, you need to install ImageMagick and ghostscript (fonts) first, you can use brew to simplify process:"
 
 if [[ "$convertValidation" = true ]]; then
-echo "安装命令: brew install imagemagick"
+echo "brew install imagemagick"
 fi
 if [[ "$gsValidation" = true ]]; then
-echo "安装命令: brew install ghostscript"
+echo "brew install ghostscript"
 fi
 exit 0;
 fi
 
-# 说明
-# commit     git-提交哈希值
-# branch     git-分支名
-# version    app-版本号
-# build_num  app-构建版本号
-
 version=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${CONFIGURATION_BUILD_DIR}/${INFOPLIST_PATH}"`
 build_num=`/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${CONFIGURATION_BUILD_DIR}/${INFOPLIST_PATH}"`
 
-# 检查当前所处Git分支
+# Check if we are under a Git or Hg repo
 if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
 commit=`git rev-parse --short HEAD`
 branch=`git rev-parse --abbrev-ref HEAD`
@@ -46,11 +40,19 @@ commit=`hg identify -i`
 branch=`hg identify -b`
 fi;
 
+#SRCROOT=..
+#CONFIGURATION_BUILD_DIR=.
+#UNLOCALIZED_RESOURCES_FOLDER_PATH=.
+
+#commit="3783bab"
+#branch="master"
+#version="3.4"
+#build_num="9999"
 
 shopt -s extglob
 build_num="${build_num##*( )}"
 shopt -u extglob
-caption="${version}($build_num)\n${branch}\n${commit}"
+caption="${version} ($build_num)\n${branch}\n${commit}"
 echo $caption
 
 function abspath() { pushd . > /dev/null; if [ -d "$1" ]; then cd "$1"; dirs -l +0; else cd "`dirname \"$1\"`"; cur_dir=`dirs -l +0`; if [ "$cur_dir" == "/" ]; then echo "$cur_dir`basename \"$1\"`"; else echo "$cur_dir/`basename \"$1\"`"; fi; fi; popd > /dev/null; }
@@ -82,36 +84,37 @@ echo "found previous file at path ${stored_original_file}, using it as base"
 mv "${stored_original_file}" "${base_path}"
 fi
 
-# Release不执行
 if [ $CONFIGURATION = "Release" ]; then
 cp "${base_path}" "$target_path"
 return 0;
 fi
 
 echo "Reverting optimized PNG to normal"
-# 命名规范
+# Normalize
 echo "xcrun -sdk iphoneos pngcrush -revert-iphone-optimizations -q ${base_path} ${base_tmp_normalizedFilePath}"
 xcrun -sdk iphoneos pngcrush -revert-iphone-optimizations -q "${base_path}" "${base_tmp_normalizedFilePath}"
 
-# 将原图移动到空文件
+# move original pngcrush png to tmp file
 echo "moving pngcrushed png file at ${base_path} to ${stored_original_file}"
 #rm "$base_path"
 mv "$base_path" "${stored_original_file}"
 
-# 将原图按照命名规范重命名
+# Rename normalized png's filename to original one
 echo "Moving normalized png file to original one ${base_tmp_normalizedFilePath} to ${base_path}"
 mv "${base_tmp_normalizedFilePath}" "${base_path}"
 
 width=`identify -format %w ${base_path}`
 height=`identify -format %h ${base_path}`
-band_height=$((($height * 50) / 100))
+band_height=$((($height * 47) / 100))
 band_position=$(($height - $band_height))
-text_position=$(($band_position - 8))
-point_size=$(((12 * $width) / 100))
+text_position=$(($band_position - 3))
+point_size=$(((13 * $width) / 100))
 
 echo "Image dimensions ($width x $height) - band height $band_height @ $band_position - point size $point_size"
 
-# 高斯模糊和文本
+#
+# blur band and text
+#
 convert ${base_path} -blur 10x8 /tmp/blurred.png
 convert /tmp/blurred.png -gamma 0 -fill white -draw "rectangle 0,$band_position,$width,$height" /tmp/mask.png
 convert -size ${width}x${band_height} xc:none -fill 'rgba(0,0,0,0.2)' -draw "rectangle 0,0,$width,$band_height" /tmp/labels-base.png
@@ -122,11 +125,13 @@ convert ${base_path} /tmp/blurred.png /tmp/mask.png -composite /tmp/temp.png
 rm /tmp/blurred.png
 rm /tmp/mask.png
 
-# 图片合成
+#
+# compose final image
+#
 filename=New${base_file}
 convert /tmp/temp.png /tmp/labels-base.png -geometry +0+$band_position -composite /tmp/labels.png -geometry +0+$text_position -geometry +${w}-${h} -composite "${target_path}"
 
-# 清除文件
+# clean up
 rm /tmp/temp.png
 rm /tmp/labels-base.png
 rm /tmp/labels.png
@@ -148,6 +153,16 @@ else
 processIcon "${icon}.png"
 processIcon "${icon}@2x.png"
 processIcon "${icon}@3x.png"
+
+processIcon "${icon}~ipad.png"
+processIcon "${icon}@2x~ipad.png"
 fi
 let i=i+1
 done
+
+# Workaround to fix issue#16 to use wildcard * to actually find the file
+# Only 72x72 and 76x76 that we need for ipad app icons
+#processIcon "AppIcon72x72~ipad*"
+#processIcon "AppIcon72x72@2x~ipad*"
+#processIcon "AppIcon76x76~ipad*"
+#processIcon "AppIcon76x76@2x~ipad*"
